e9f25f0d502d6ba0aa620653a715158dd653b501
Laura <181015028+LauraVargas22@users.noreply.github.com.>
2025-03-19 18:38:13 -0500

    feat: :sparkles: Query

---
+++ insert.sql
@@ -0,0 +1,12 @@
+INSERT INTO usuarios (nombre, email, contrasena)
+VALUES ('Juan Pérez', 'juan.perez@example.com', 'password123'),
+('María López', 'maria.lopez@example.com', 'maria1234'),
+('Carlos Martínez', 'carlos.martinez@example.com', 'carlo_pass'),
+('Ana Fernández', 'ana.fernandez@example.com', 'ana2023'),
+('Luis Gómez', 'luis.gomez@example.com', 'luis_pass'),
+('Lucía Rivera', 'lucia.rivera@example.com', 'lucia2023'),
+('Pedro Ortiz', 'pedro.ortiz@example.com', 'pedro_pass'),
+('Sofía Blanco', 'sofia.blanco@example.com', 'sofiablanco123'),
+('Miguel Rojas', 'miguel.rojas@example.com', 'miguerojas2023'),
+('Carla Ruiz', 'carla.ruiz@example.com', 'carlitaaruiz');
+
